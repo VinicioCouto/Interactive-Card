4ebd72d20b76cc5b7c7da188b666f5e2733d4f39
Vinicio Couto <viniciogbi@gmail.com>
2025-02-01 09:16:07 -0300

    variaveis js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,13 @@
-const nomeTitular 
+const nomeTitular = document.querySelector('.nome')
+const numeroCartao = document.querySelector('.numero')
+const dataExpd = document.querySelector('.data-expd')
+const cvc = document.querySelector('.cvc')
+
+const inputNome = document.querySelector('#nome').valure
+const inputNumero = document.querySelector('#numero').valure
+const inputMes = document.querySelector('#mes').valure
+const inputAno = document.querySelector('#ano').valure
+const inputCvc = document.querySelector('#cvc').valure
+
+
+nomeTitular.innerHTML
